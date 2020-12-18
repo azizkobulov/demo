bc9085b59f458f02bbbc6d52eef3ea80f20a62e1
Aziz <example@gmail.com>
2020-12-18 22:38:17 +0500

    l,ls,dl;as,d;las,d

--- src/js/script.js
+++ src/js/script.js
@@ -101,6 +101,35 @@
       
       $('input[name=number]').mask(" (99) 999 -- 99 -- 99");
 
+      $('form').submit(function(e){
+          e.preventDefault();
+          $.ajax({
+              type: "POST",
+              url: "mailer/smart.php",
+              data: $(this).serialize()
+          }).done(function(){
+              $(this).find("input").val("");
+
+              $('form').trigger('reset');
+          });
+          return false;
+      });
+
+      $(window).scroll(function(){
+          if($(this).scrollTop()>1600) {
+            $('.pageup').fadeIn()
+          }
+          else {
+            $('.pageup').fadeOut()
+          }
+      });
+
+      $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+
 }); 
  /*  var slider = tns({
     container: '.carousel__inner',
